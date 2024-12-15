4a4774da3aedbcc5e59a5e47fb79054898be9ca4
Denis Rodionov <alax777kilden@yandex.ru>
2024-12-15 23:37:45 +0700

    Config

--- src/main/kotlin/application/telegram/TelegramBotConfig.kt
+++ src/main/kotlin/application/telegram/TelegramBotConfig.kt
@@ -1,6 +1,25 @@
 package application.telegram
 
+import java.io.File
+import java.util.Properties
+
 object TelegramBotConfig {
-    val ACCESS_TOKEN: String = System.getenv("TELEGRAM_ACCESS_TOKEN")
-        ?: throw IllegalStateException("TELEGRAM_ACCESS_TOKEN is not set in the environment variables")
+    val ACCESS_TOKEN: String by lazy {
+        System.getenv("TELEGRAM_ACCESS_TOKEN")
+            ?: loadFromLocalFile()
+            ?: throw IllegalStateException(
+                "TELEGRAM_ACCESS_TOKEN is not set in the environment variables or local.properties file"
+            )
+    }
+
+    private fun loadFromLocalFile(): String? {
+        val configFile = File("local.properties")
+        if (configFile.exists()) {
+            val properties = Properties().apply {
+                load(configFile.inputStream())
+            }
+            return properties.getProperty("TELEGRAM_ACCESS_TOKEN")
+        }
+        return null
+    }
 }
